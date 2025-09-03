cc1b6e5ac6ecc898ff3a21dd94d850e1f62a1753
nelsw <connorvanelswyk@gmail.com>
2025-09-03 19:10:48 -0400

    add missing methods to request.go

--- pkg/api/request.go
+++ pkg/api/request.go
@@ -7,9 +7,25 @@ import (
 )
 
 func IsOptions(req events.LambdaFunctionURLRequest) bool {
-	return req.RequestContext.HTTP.Method == http.MethodOptions
+	return is(req, http.MethodOptions)
 }
 
 func IsPost(req events.LambdaFunctionURLRequest) bool {
-	return req.RequestContext.HTTP.Method == http.MethodPost
+	return is(req, http.MethodPost)
+}
+
+func IsGet(req events.LambdaFunctionURLRequest) bool {
+	return is(req, http.MethodGet)
+}
+
+func IsPatch(req events.LambdaFunctionURLRequest) bool {
+	return is(req, http.MethodPatch)
+}
+
+func IsDelete(req events.LambdaFunctionURLRequest) bool {
+	return is(req, http.MethodDelete)
+}
+
+func is(req events.LambdaFunctionURLRequest, method string) bool {
+	return req.RequestContext.HTTP.Method == method
 }
