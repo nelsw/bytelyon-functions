aee080664a2be2a1ed97bba76dd7d5a00ded0489
nelsw <connorvanelswyk@gmail.com>
2025-09-03 19:10:57 -0400

    clean up response file

--- pkg/api/response.go
+++ pkg/api/response.go
@@ -1,91 +1,62 @@
 package api
 
 import (
+	"bytelyon-functions/internal/util"
 	"encoding/json"
-	"log"
+	"errors"
+	"fmt"
 	"net/http"
-	"os"
-	"regexp"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/rs/zerolog/log"
 )
 
-var appModeRegex *regexp.Regexp
-
-func init() {
-	appModeRegex = regexp.MustCompile("^(local|debug)$")
+func logMap(s string, m map[string]interface{}) {
+	log.Info().Any(s, m).Send()
 }
 
-func Log(v ...interface{}) {
-	if v == nil || len(v) == 0 {
-		return
-	}
-	var m = map[string]interface{}{}
-	for i := 0; i < len(v); i++ {
-		if i%2 == 0 {
-			m[v[i].(string)] = nil
-		} else {
-			m[v[i-1].(string)] = v[i]
-		}
-	}
-	LogMap(m)
+func okStr(s string) (events.LambdaFunctionURLResponse, error) {
+	return response(http.StatusOK, s)
 }
 
-func LogMap(m map[string]interface{}) {
-	if appModeRegex.MatchString(os.Getenv("APP_MODE")) {
-		return
-	}
-	b, _ := json.Marshal(m)
-	log.Println(string(b))
-}
-
-func LogURLRequest(req events.LambdaFunctionURLRequest) {
-	LogMap(map[string]interface{}{
+func requestMap(req events.LambdaFunctionURLRequest) map[string]any {
+	m := map[string]any{
 		"headers": req.Headers,
 		"method":  req.RequestContext.HTTP.Method,
+		"path":    req.RequestContext.HTTP.Path,
 		"query":   req.QueryStringParameters,
-		"body":    req.Body,
-	})
-}
-
-func OK(v ...any) (events.LambdaFunctionURLResponse, error) {
-	if v == nil || len(v) == 0 || v[0] == nil {
-		return Response(http.StatusOK, "")
 	}
-	if s, ok := v[0].(string); ok {
-		return Response(http.StatusOK, s)
+	if req.IsBase64Encoded == false && util.IsJSON(req.Body) {
+		var a map[string]any
+		_ = json.Unmarshal([]byte(req.Body), &a)
+		m["body"] = a
+	} else {
+		m["body"] = req.Body
 	}
-	b, err := json.Marshal(v[0])
-	if err != nil {
-		b, _ = json.Marshal(map[string]interface{}{
-			"message": "failed to marshal response",
-			"error":   err.Error(),
-		})
-		return Response(http.StatusInternalServerError, string(b))
-	}
-	return Response(http.StatusOK, string(b))
+	return m
 }
 
-func BadRequest(err error) (events.LambdaFunctionURLResponse, error) {
-	return Response(http.StatusBadRequest, err.Error())
+func requestMapString(req events.LambdaFunctionURLRequest) string {
+	m := requestMap(req)
+	b, _ := json.Marshal(&m)
+	return string(b)
 }
 
-func ServerError(err error) (events.LambdaFunctionURLResponse, error) {
-	return Response(http.StatusInternalServerError, err.Error())
-}
-
-func NotImplemented(s string) (events.LambdaFunctionURLResponse, error) {
-	return Response(http.StatusNotImplemented, s)
-}
-
-func Response(code int, body string) (events.LambdaFunctionURLResponse, error) {
+func response(code int, body string) (events.LambdaFunctionURLResponse, error) {
 
 	// log the response so we have full visibility into how the request was handled
-	Log("code", code, "body", body)
+	m := map[string]any{"code": code}
+
+	var a map[string]any
+	if err := json.Unmarshal([]byte(body), &a); err != nil {
+		m["body"] = body
+	} else {
+		m["body"] = a
+	}
+
+	logMap("response", m)
 
 	// return the given ƒ response with a few header values that are required when you QD an API route
-	// note that we always return nil for error because if we don't, we'll always return a 500
-	// as I understand it, it's a worse case scenario akin to self-destruct mode
 	return events.LambdaFunctionURLResponse{
 		Headers: map[string]string{
 			"Access-Control-Allow-Origin":  "*",
@@ -95,4 +66,59 @@ func Response(code int, body string) (events.LambdaFunctionURLResponse, error) {
 		StatusCode: code,
 		Body:       body,
 	}, nil
+}
+
+func BadRequest(err error) (events.LambdaFunctionURLResponse, error) {
+	return response(http.StatusBadRequest, err.Error())
+}
+
+func LogURLRequest(req events.LambdaFunctionURLRequest) {
+	m := map[string]any{
+		"headers": req.Headers,
+		"method":  req.RequestContext.HTTP.Method,
+		"path":    req.RequestContext.HTTP.Path,
+		"query":   req.QueryStringParameters,
+	}
+	if req.IsBase64Encoded == false && util.IsJSON(req.Body) {
+		var a map[string]any
+		_ = json.Unmarshal([]byte(req.Body), &a)
+		m["body"] = a
+	} else {
+		m["body"] = req.Body
+	}
+	logMap("request", requestMap(req))
+}
+
+func Marshall(a any) (events.LambdaFunctionURLResponse, error) {
+	b, err := json.Marshal(a)
+	if err != nil {
+		return ServerError(errors.Join(errors.New(fmt.Sprintf("failed to marshal %#v", a)), err))
+	}
+	return okStr(string(b))
+}
+
+func NotImplemented(req events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
+	return response(http.StatusNotImplemented, requestMapString(req))
+}
+
+func OK() (events.LambdaFunctionURLResponse, error) {
+	return okStr("")
+}
+
+func Response(b []byte, err error) (events.LambdaFunctionURLResponse, error) {
+
+	if err != nil {
+		return BadRequest(err)
+	}
+
+	var s string
+	if b != nil {
+		s = string(b)
+	}
+
+	return okStr(s)
+}
+
+func ServerError(err error) (events.LambdaFunctionURLResponse, error) {
+	return response(http.StatusInternalServerError, err.Error())
 }
