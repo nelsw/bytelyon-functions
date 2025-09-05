0c3634ad3ca829e0b30902dfab68962dbae91a03
nelsw <connorvanelswyk@gmail.com>
2025-09-05 06:05:27 -0400

    add coverage

--- internal/handler/jwt/handler_test.go
+++ internal/handler/jwt/handler_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// expired token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiMDFLNDhQQzBCSzEzQldWMkNHV0ZQOFFRSDAifSwiaXNzIjoiQnl0ZUx5b24iLCJleHAiOjE3NTcwMTgxNDMsIm5iZiI6MTc1NzAxNjM0MywiaWF0IjoxNzU3MDE2MzQzLCJqdGkiOiIwYWVlNDdjMy03YTQ2LTRjYmQtYTdhYy1jNzQ2NjBmODg0MjQifQ.04abFJOZf-qB1C-C2y7Pjj4c2krkAyxCDZy7SK7p3Y4
+
 func TestBadType(t *testing.T) {
 	t.Setenv("JWT_SECRET", "a-string-secret-at-least-256-bits-long")
 	res, err := Handler(model.JWTRequest{})
