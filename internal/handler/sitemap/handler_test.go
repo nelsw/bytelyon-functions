fd2232086c4c101f703a46b7ebaa526277e825ce
celswyk <celswyk@ubicquia.com>
2025-09-11 11:43:29 -0400

    anonymize test

--- internal/handler/sitemap/handler_test.go
+++ internal/handler/sitemap/handler_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"bytelyon-functions/internal/app"
 	"bytelyon-functions/internal/model"
 	"bytelyon-functions/test"
 	"context"
-	"github.com/oklog/ulid/v2"
 	"testing"
 )
 
 func Test_Handler(t *testing.T) {
 	test.Init(t)
-	u := model.User{ID: ulid.MustParse("01K48PC0BK13BWV2CGWFP8QQH0")}
+	u := model.User{ID: app.NewUlid()}
 	Handler(context.Background(), Request{
 		UserID: u.ID,
 		Depth:  5,
