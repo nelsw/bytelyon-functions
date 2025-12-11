f5a05df0d6549bb2ab4270a5558d68a8e804a4c0
nelsw <connorvanelswyk@gmail.com>
2025-12-10 20:58:06 -0500

    replace space with +

--- pkg/model/news.go
+++ pkg/model/news.go
@@ -140,13 +140,15 @@ func (n *News) Work() {
 	}
 
 	q := strings.Join(n.Keywords, "+")
+	q = strings.ReplaceAll(q, " ", "+")
+
 	urls := []string{
 		fmt.Sprintf("https://news.google.com/rss/search?q=%s&hl=en-US&gl=US&ceid=US:en", q),
 		fmt.Sprintf("https://www.bing.com/news/search?format=rss&q=%s", q),
 		fmt.Sprintf("https://www.bing.com/search?format=rss&q=%s", q),
 	}
 
-	var items []Item
+	var items []*Item
 	for _, u := range urls {
 
 		rss, err := NewRSS(u)
